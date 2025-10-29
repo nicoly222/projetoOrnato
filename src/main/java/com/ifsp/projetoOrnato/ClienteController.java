dba2c71a00e2759b4963961f43f74c4ef2c7d69c
nicoly222 <f.nicoly@aluno.ifsp.edu.br>
2025-10-29 07:39:44 -0300

    templates adicionado

--- src/main/java/com/ifsp/projetoOrnato/ClienteController.java
+++ src/main/java/com/ifsp/projetoOrnato/ClienteController.java
@@ -1,39 +1,30 @@
-import javax.management.loading.ClassLoaderRepository;
+package com.ifsp.projetoOrnato;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import com.ifsp.projetoOrnato.cliente;
 
+@Controller
+@RequestMapping("/login")
 public class ClienteController {
 
-    @Controller
-    @RequestMapping ("/login")
-    public class UsuarioController {
-        
-        private final ClassLoaderRepository clienteRepo;
+    private final ClienteRepository usuarioRepo;
 
-        public UsuarioController (ClassLoaderRepository clienteRepo) {
-            this.clienteRepo = clienteRepo;
-        }
-        @GetMapping
-         public String login (){
-            return "login";
-         }
-
-         @PostMapping
-         public String autenticar (@RequestParam String email, @RequestParam String senha){
-
-            cliente cliente = ((Object) clienteRepo).findByEmail(email);
-           
-            if (cliente != null && cliente.getSenha().equals(senha)) {
-            return "redirect:/";
-          }
-          return "redirect:/login?erro";
-         }
+    public ClienteController(ClienteRepository clienteRepo) {
+        this.clienteRepo = clienteRepoRepo;
     }
-    
+
+    @GetMapping
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping
+    public String autenticar(@RequestParam String email, @RequestParam String senha) {
+        Cliente cliente = clienteRepo.findByEmail(email);
+        if (usuario != null && usuario.getSenha().equals(senha)) {
+            return "redirect:/";
+        }
+        return "redirect:/login?erro";
+    }
 }
